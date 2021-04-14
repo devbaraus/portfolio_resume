29c3fd1fcd06ccbd9a860f6b48ac79e5e242ed0f
devbaraus <me@baraus.dev>
2021-04-13 23:53:45 -0300

    Resume Initial

--- puppetter.js
+++ puppetter.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer')
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
 
-  await page.goto('http://localhost:8080/print/', {
+  await page.goto((process.env.URL || 'http://localhost:8080') + '/resume.pdf', {
     waitUntil: 'networkidle0',
   })
 
