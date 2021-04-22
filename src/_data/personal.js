4a9f7a66f971864feeabb658cbcfe85dc26fcba3
devbaraus <me@baraus.dev>
2021-04-22 20:36:07 -0300

    Alteração no redirect para website

--- src/_data/personal.js
+++ src/_data/personal.js
@@ -1,18 +1,31 @@
 module.exports = {
-  phone: '(62) 9 99120-6888',
-  email: 'me@baraus.dev',
-  competences: ['JavaScript', 'PHP', 'Python', 'Ruby', 'Docker', 'PostgreSQL', 'Adobe Illustrator', 'Adobe Photoshop', 'Web Design'],
-  socials: [
-    { label: 'Website', link: 'https://baraus.dev' },
-    { label: 'Linkedin', link: 'https://linkedin.com/in/devbaraus' },
-    { label: 'GitHub', link: 'https://github.com/devbaraus' },
-    { label: 'Instagram', link: 'https://instagram.com/devbaraus' },
-  ],
-  works: [
-    { label: 'Code Tower', link: 'https://codetower.com.br/' },
-    { label: 'IFG Audio', link: 'https://ifgaudio.bcc.anapolis.ifg.edu.br/' },
-    { label: 'Comissão Própria de Avaliação', link: 'https://cpa.bcc.anapolis.ifg.edu.br' },
-    { label: 'IFG Produz', link: 'https://ifgproduz.ifg.edu.br' },
-    { label: 'Inteligência Artificial', link: 'https://ia.ifg.edu.br' },
-  ],
+	phone: '(62) 9 99120-6888',
+	email: 'me@baraus.dev',
+	competences: [
+		'JavaScript',
+		'PHP',
+		'Python',
+		'Ruby',
+		'Docker',
+		'PostgreSQL',
+		'Adobe Illustrator',
+		'Adobe Photoshop',
+		'Web Design',
+	],
+	socials: [
+		{ label: 'Website', link: 'https://baraus.dev/?ref=resume' },
+		{ label: 'Linkedin', link: 'https://linkedin.com/in/devbaraus' },
+		{ label: 'GitHub', link: 'https://github.com/devbaraus' },
+		{ label: 'Instagram', link: 'https://instagram.com/devbaraus' },
+	],
+	works: [
+		{ label: 'Code Tower', link: 'https://codetower.com.br/' },
+		{ label: 'IFG Audio', link: 'https://ifgaudio.bcc.anapolis.ifg.edu.br/' },
+		{
+			label: 'Comissão Própria de Avaliação',
+			link: 'https://cpa.bcc.anapolis.ifg.edu.br',
+		},
+		{ label: 'IFG Produz', link: 'https://ifgproduz.ifg.edu.br' },
+		{ label: 'Inteligência Artificial', link: 'https://ia.ifg.edu.br' },
+	],
 }
